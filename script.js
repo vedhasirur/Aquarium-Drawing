246960811827511715796ab9ee7fe70798e60682
vedha <79853394+vedhasirur@users.noreply.github.com>
2021-10-03 18:56:28 +0000

    added javascript

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html> 
- <head>
-    <title>Processing.JS inside Webpages: Template</title> 
-</head>
- <body>
-    <p align="center"> 
-	<!--This draws the Canvas on the webpage -->
-      <canvas id="mycanvas"></canvas> 
-    </p>
- </body>
- 
- <!-- Run all the JavaScript stuff -->
- <!-- Include the processing.js library -->
- <!-- See https://khanacademy.zendesk.com/hc/en-us/articles/202260404-What-parts-of-ProcessingJS-does-Khan-Academy-support- for differences -->
- <script src="https://cdn.jsdelivr.net/processing.js/1.4.8/processing.min.js"></script> 
- 
- <p class= "tank"><script>
+
     var sketchProc = function(processingInstance) {
      with (processingInstance) {
         size(400, 400); 
@@ -87,6 +70,3 @@ var mouseClicked= function (){
     var canvas = document.getElementById("mycanvas"); 
     // Pass the function sketchProc (defined in myCode.js) to Processing's constructor.
     var processingInstance = new Processing(canvas, sketchProc); 
- </script>
-</p>
-</html>
